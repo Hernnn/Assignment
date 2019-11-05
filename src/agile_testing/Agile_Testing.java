64426f86c42f2ef94b2594458640c5fc73014a1a
B1 <B1@B140>
2019-11-05 08:32:39 +0800

    Completed Story #2342341423

---
+++ src/agile_testing/Agile_Testing.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agile_testing;
+
+/**
+ *
+ * @author B1
+ */
+public class Agile_Testing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+        System.out.println("Good Bye!");
+        System.out.println("Thank You");
+        System.out.println("Blue Cylinder is different");
+    }
+    
+}
